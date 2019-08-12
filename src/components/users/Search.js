796d1e05e4a8a2179ad5776817be6a35fdb6dd97
Peter Sims <ragtimesims@gmail.com>
2019-08-11 21:54:19 -0400

    refactoring

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,7 +6,9 @@ class Search extends Component {
 		text: ''
 	};
 	static propTypes = {
-		searchUsers: PropTypes.func.isRequired
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired
 	};
 
 	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
@@ -18,6 +20,7 @@ class Search extends Component {
 	};
 
 	render() {
+		const { clearUsers, showClear } = this.props;
 		return (
 			<div>
 				<form className="form" onSubmit={this.onSubmit}>
@@ -30,6 +33,11 @@ class Search extends Component {
 					/>
 					<input type="submit" value="Search" className="btn btn-dark btn-block" />
 				</form>
+				{showClear && (
+					<button className="btn btn-light btn-block" onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
